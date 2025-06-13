698b66b4b55168913242913566f153ead0b6b9ab
Shynggys <shynggys.zhakenuly@gmail.com>
2025-06-13 23:45:54 +0500

    Updated middleware.ts

--- lib/supabase/middleware.ts
+++ lib/supabase/middleware.ts
@@ -40,12 +40,13 @@ export async function updateSession(request: NextRequest) {
   if (
     !user &&
     !request.nextUrl.pathname.startsWith('/login') &&
-    !request.nextUrl.pathname.startsWith('/auth')
+    !request.nextUrl.pathname.startsWith('/auth') &&
+    !request.nextUrl.pathname.startsWith('/api/')
   ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone()
     url.pathname = '/auth/login'
-    return NextResponse.redirect(url)
+    return NextResponse.redirect(url.toString())
   }
 
   // IMPORTANT: You *must* return the supabaseResponse object as it is.
