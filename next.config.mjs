fd316e353510bfcedb952ebef734e7b5204d8dd3
Shynggys <shynggys.zhakenuly@gmail.com>
2025-06-13 23:43:15 +0500

    Updated next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  eslint: {
-    ignoreDuringBuilds: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.replica.tavus.io',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'tavus-assets.s3.amazonaws.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'example.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
-  images: { unoptimized: true },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
